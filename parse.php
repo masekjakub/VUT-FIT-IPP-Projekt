06acd6126abd8248a5102437f9c89b7ee4ca2446
masekjakub <masekjak02@gmail.com>
2023-02-15 23:56:57 +0100

    minor stats fixes

--- parse.php
+++ parse.php
@@ -20,16 +20,35 @@ $longopts  = array(
     "comments",
     "labels",
     "jumps",
+    "fwjumps",
+    "backjumps",
+    "badjumps",
     "print:",
 
 );
 $opts = getopt($shortopts, $longopts, $index);
 
-if ((isset($opts["help"]) || isset($opts["h"])) && count($opts) == 1) {
+if (isset($opts["help"]) || isset($opts["h"])) {
+    if(count($opts) != 1){
+        fwrite(STDERR, "Help option cannot be combinated with another one! Try --help\n");
+        exit(myError::E_WRONGPARAM->value);
+    }
     echo "Usage:\n";
-    echo "php parse.php [-h]\n\n";
-    echo "Description:\n";
+    echo "\tphp parse.php [options]\n\n";
+    echo "Options:\n";
+    echo "--help\t\t\tPrints this help.\n";
+    echo "--stats=file\t\tSet file to print statistics to. Has to be set before following options.\n";
+    echo "--loc\t\t\tPrints number of lines.\n";
+    echo "--comments\t\tPrints number of comments.\n";
+    echo "--labels\t\tPrints number of labels.\n";
+    echo "--jumps\t\t\tPrints number of jumps.\n";
+    echo "--fwjumps\t\tPrints number of forward jumps.\n";
+    echo "--backjumps\t\tPrints number of backward jumps.\n";
+    echo "--badjumps\t\tPrints number of bad jumps.\n";
+    echo "--print=\"string\"\tPrints string representation to file.\n";
+    echo "\nDescription:\n";
     echo "Script reads IPPcode23 code from STDIN, checks syntax and generates XML representation to STDOUT.\n";
+    echo "Every statistic is printed to file set before. Multiple files are possible.\n";
     echo "\nJakub Mašek, 2023, VUT FIT\n";
     exit(myError::E_OK->value);
 }
@@ -58,45 +77,35 @@ for ($i = 1; $i < $argc; $i++) {
         $statsFile = fopen("$fileName", "w") or die("Unable to open file!");
         $statsFileSet = 1;
     }
-
     if ($statsFileSet == 0) {
         fwrite(STDERR, "Mising file to write to! Try --help\n");
         exit(myError::E_WRONGPARAM->value);
     }
-
     if (preg_match("/^--print=/", $argv[$i])) {
         $value = preg_replace("/^--print=/", "", $argv[$i]);
         fwrite($statsFile, "$value\n");
     }
-
     if ($argv[$i] == "--loc") {
         fwrite($statsFile, $statsArr["loc"] . "\n");
     }
-
     if ($argv[$i] == "--comments") {
         fwrite($statsFile, $statsArr["comments"] . "\n");
     }
-
     if ($argv[$i] == "--labels") {
         fwrite($statsFile, $statsArr["labels"] . "\n");
     }
-
     if ($argv[$i] == "--jumps") {
         fwrite($statsFile, $statsArr["jumps"] . "\n");
     }
-
     if ($argv[$i] == "--fwjumps") {
         fwrite($statsFile, $statsArr["fwJumps"] . "\n");
     }
-
     if ($argv[$i] == "--backjumps") {
         fwrite($statsFile, $statsArr["backJumps"] . "\n");
     }
-
     if ($argv[$i] == "--badjumps") {
         fwrite($statsFile, $statsArr["badJumps"] . "\n");
     }
-
     if ($argv[$i] == "--eol") {
         fwrite($statsFile, "\n");
     }
